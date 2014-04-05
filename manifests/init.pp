74e48af75996459214a8d6e0030fa4d5293b0a40
Marc Villacorta <marc.villacorta@gmail.com>
2014-04-05 14:11:17 +0200

    Use of contain function

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,4 @@
 class dnsmasq {
 
-    anchor { "${module_name}::begin":   } ->
-    class  { "${module_name}::install": } ->
-    anchor { "${module_name}::end":     }
+    contain ${module_name}::install
 }
