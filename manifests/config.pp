08dfc467e8488bf7aeee12f9639df69dfd7339ac
Marc Villacorta <marc.villacorta@gmail.com>
2014-04-05 14:27:03 +0200

    Fix typo

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,11 +1,11 @@
 class dnsmasq::config {
 
     file { '/etc/dnsmasq.conf':
-        ensure   => present,
-        content  => template("${module_name}/dnsmasq.conf.erb"),
-        owner    => 'root',
-        group    => 'root',
-        mode     => '0644',
-        requires => Package['dnsmasq'],
+        ensure  => present,
+        content => template("${module_name}/dnsmasq.conf.erb"),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        require => Package['dnsmasq'],
     }
 }
