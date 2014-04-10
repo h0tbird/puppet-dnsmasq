8091acf830aff79f0ece38d2225663cabfed4a70
Marc Villacorta <marc.villacorta@gmail.com>
2014-04-10 23:06:46 +0200

    Adding supervisord.conf.erb

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,9 +1,10 @@
 class dnsmasq::service inherits dnsmasq {
 
     file { '/etc/supervisord.conf':
-        ensure => present,
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0644',
+        ensure  => present,
+        content => template("$module_name/supervisord.conf.erb");
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
     }
 }
