3a8652dca6ea6414eeb35fff6c9242d725b1bdbb
Marc Villacorta <marc.villacorta@gmail.com>
2014-04-27 21:14:52 +0200

    Addin dhcp_range parameter

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,7 @@ class dnsmasq (
     $dnsmasq_version       = 'latest',
     $dnsmasq_effective_uid = 'root',
     $dnsmasq_effective_gid = 'root',
+    $dhcp_range            = ['192.168.1.50,192.168.1.150,12h'],
 
 ) {
 
