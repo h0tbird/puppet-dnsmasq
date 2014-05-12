656e436c9da5135fba8c3fdf5fe94d3280d63acf
Marc Villacorta <marc.villacorta@gmail.com>
2014-05-12 22:06:35 +0200

    Setup supervisord

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,6 +1,6 @@
 class dnsmasq::service inherits dnsmasq {
 
-    file { '/etc/supervisord.conf':
+    file { "/etc/supervisord.d/${modulename}.conf":
         ensure  => present,
         content => template("$module_name/supervisord.conf.erb"),
         owner   => 'root',
