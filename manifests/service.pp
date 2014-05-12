7872cea331cdd2a2627d6029109ad9a372a5bab4
Marc Villacorta <marc.villacorta@gmail.com>
2014-05-12 22:23:42 +0200

    Fix config file name

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,8 +1,8 @@
 class dnsmasq::service inherits dnsmasq {
 
-    file { "/etc/supervisord.d/${modulename}.conf":
+    file { "/etc/supervisord.d/${module_name}.conf":
         ensure  => present,
-        content => template("$module_name/supervisord.conf.erb"),
+        content => template("${module_name}/supervisord.conf.erb"),
         owner   => 'root',
         group   => 'root',
         mode    => '0644',
