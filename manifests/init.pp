8e040b773af894f0878e24ffbc066e4eb80e7ff1
Marc Villacorta <marc.villacorta@gmail.com>
2014-08-06 21:53:48 +0200

    Adding enable_tftp and tftp_root

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,6 +8,8 @@ class dnsmasq (
   $dhcp_range    = ['192.168.1.50,192.168.1.150,12h'],
   $log_queries   = undef,
   $log_dhcp      = undef,
+  $enable_tftp   = undef,
+  $tftp_root     = '/var/ftpd',
 
 ) {
 
