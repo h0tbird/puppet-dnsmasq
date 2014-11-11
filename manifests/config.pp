6bbc1166aaa26a1a522114df00d9577083015f37
Marc Villacorta <marc.villacorta@gmail.com>
2014-11-11 20:11:35 +0100

    Populate addn_hosts file

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,11 +1,21 @@
 class dnsmasq::config inherits dnsmasq {
 
-  file { '/etc/dnsmasq.conf':
-    ensure  => present,
-    content => template("${module_name}/dnsmasq.conf.erb"),
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    require => Package['dnsmasq'],
+  file {
+
+    '/etc/dnsmasq.conf':
+      ensure  => present,
+      content => template("${module_name}/dnsmasq.conf.erb"),
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      require => Package['dnsmasq'];
+
+    '/usr/lib/systemd/system/dnsmasq.service':
+      ensure  => present,
+      content => template("${module_name}/dnsmasq.service.erb"),
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      require => Package['dnsmasq'];
   }
 }
